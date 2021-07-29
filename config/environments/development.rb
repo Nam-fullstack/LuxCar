7e05556b22c225603870cc2997ea9c2b73fd0829
Nam-fullstack <namnguyen13_@hotmail.com>
2021-07-29 12:30:41 +1000

    NEED TO CHANGE ACTION MAILER SETTINGS FOR LIVE APP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  # setups default url for devise development mailer
+  # *************************************************************************
+  # will NEED TO CHANGE the host url to deployment platform for live app 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
