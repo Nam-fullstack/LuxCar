3d1fe687563c15f10bca16b69780f9f2905d9614
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-02 16:33:34 +1000

    Creates Year Make Model models and migrations

---
+++ db/migrate/20210802063256_create_models.rb
@@ -0,0 +1,11 @@
+class CreateModels < ActiveRecord::Migration[6.1]
+  def change
+    create_table :models do |t|
+      t.string :name
+      t.references :make, null: false, foreign_key: true
+      t.references :variant, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
