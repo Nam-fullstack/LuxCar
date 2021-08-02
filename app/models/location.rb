2e1a63672555a5c11064febad2ae8f1e33ac32e2
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-02 20:01:27 +1000

    Adds validations for location model.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,8 @@
 class Location < ApplicationRecord
-  belongs_to :event
+  belongs_to :event, dependent: :destroy
+
+  # Validations
+  validates :postcode, presence: true, length: { is: 4 }, numericality: { greater_than: 0200, less_than: 9999 }
+  validates :address, presence: true
+
 end
