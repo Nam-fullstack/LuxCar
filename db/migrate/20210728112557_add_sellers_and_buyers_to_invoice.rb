294b23dd6f483184a5ea250b2d591e258af0765b
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-02 12:42:00 +1000

    Minor adjustments to migrations for engines and transmissions. Checkpoint.

--- db/migrate/20210728112557_add_sellers_and_buyers_to_invoice.rb
+++
@@ -1,9 +0,0 @@
-class AddSellersAndBuyersToInvoice < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :invoices, :buyer, index: true
-    add_reference :invoices, :seller, index: true
-    
-    add_foreign_key :invoices, :users, column: :buyer_id
-    add_foreign_key :invoices, :users, column: :seller_id
-  end
-end
