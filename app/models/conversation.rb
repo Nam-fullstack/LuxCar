611a5770bfb7f287d493690875d627b7cbf26c6b
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-04 01:49:24 +1000

    Adds payment_intent validation required for Purchases, and adds method to determine if user is recipient or sender in the conversation.

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,6 @@
 class Conversation < ApplicationRecord
   # This setups up a self join within the Conversation model for the User model.
-  # Allows for a user to be both the send and recipient of messages.
+  # Allows for a user to be both the sender and recipient of messages.
   belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
   belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
   has_many :messages, dependent: :destroy
@@ -8,7 +8,12 @@ class Conversation < ApplicationRecord
   # Defines a conversation that is unique for sender based on the recipient.
   # Sender and recipient can't have multiple conversations.
   validates_uniqueness_of :sender_id, scope: :recipient_id
+  # Finds the conversation between the two users
   scope :between, ->(sender_id, recipient_id) do
     where("(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)", sender_id, recipient_id, recipient_id, sender_id)
+  end
+
+  def recipient(current_user)
+    self.sender_id == current_user.id ? self.recipient : self.sender
   end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,4 +2,6 @@ class Purchase < ApplicationRecord
   belongs_to :listing
   belongs_to :user
   has_one :invoice
+
+  validates :payment_intent, presence: true
 end
