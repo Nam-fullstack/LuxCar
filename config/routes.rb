720dd2454d935261a4703f5a1d7f42d2720435ef
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-03 11:06:57 +1000

    Re-adds username, firstname and lastname to sign-up form for users and admin.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
   resources :conversations
   resources :listings
   resources :events
-  devise_for :admins, controllers: { sessions: 'admins/sessions' }
   # skip: :all - enables current_user helper methods 
-  devise_for :users, controllers: { sessions: 'users/sessions' }, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }, skip: :all
-
+  devise_for :admins, controllers: { sessions: 'admins/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions' }#, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }, skip: :all
 
   root to: 'pages#home'
   get 'pages/restricted'
