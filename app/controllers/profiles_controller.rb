c54774ffc64264bb16b979b828be36e62fec1b34
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-04 02:15:44 +1000

    Adds some routing and extra authentication for Profiles controller.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,25 +1,23 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  before_action :authorize_iser!, only: %i[ edit destroy ]
 
-  # GET /profiles or /profiles.json
-  def index
-    @profiles = Profile.all
-  end
-
-  # GET /profiles/1 or /profiles/1.json
   def show
+    if current_user.profile.id == params[:id].to_i
+      render "user_profile" # shows the current user's profile
+    else
+      render "show" # if not the current user
+    end
   end
 
-  # GET /profiles/new
   def new
     @profile = Profile.new
   end
 
-  # GET /profiles/1/edit
   def edit
   end
 
-  # POST /profiles or /profiles.json
   def create
     @profile = Profile.new(profile_params)
 
@@ -34,9 +32,15 @@ class ProfilesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /profiles/1 or /profiles/1.json
   def update
     respond_to do |format|
+
+      # If the profile isn't edited/changed, redirects back to profile page
+      if !params[:profile]
+        redirect_to profile_path(@profile.id)
+        return
+      end
+
       if @profile.update(profile_params)
         format.html { redirect_to @profile, notice: "Profile was successfully updated." }
         format.json { render :show, status: :ok, location: @profile }
@@ -47,23 +51,30 @@ class ProfilesController < ApplicationController
     end
   end
 
-  # DELETE /profiles/1 or /profiles/1.json
   def destroy
     @profile.destroy
     respond_to do |format|
-      format.html { redirect_to profiles_url, notice: "Profile was successfully destroyed." }
+      format.html { redirect_to profiles_url, notice: "Profile was successfully deleted." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_profile
-      @profile = Profile.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def profile_params
-      params.fetch(:profile, {})
+  # Use callbacks to share common setup or constraints between actions.
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def profile_params
+    params.require(:profile).permit(:user_id, :image)
+  end
+
+  def authorize_user!
+    if current_user.id != @profile.user.id
+      flash[:alert] = "You are not authorized to view this."
+      redirect_to root_url
     end
+  end
 end
