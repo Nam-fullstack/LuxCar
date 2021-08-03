0083cc261261b96185c59916ce6b2e3c59c8be33
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-04 01:52:50 +1000

    Sanitizes Locations model.

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,4 +5,11 @@ class Location < ApplicationRecord
   validates :postcode, presence: true, length: { is: 4 }, numericality: { greater_than: 0200, less_than: 9999 }
   validates :address, presence: true
 
+  before_save :remove_whitespace
+
+  private
+
+  def remove_whitespace
+    self.address = address.strip
+  end
 end
