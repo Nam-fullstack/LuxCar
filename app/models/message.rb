80e3913b87974c50698f7d614b3120a2930d9248
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-04 01:41:24 +1000

    Data sanitization for Messages model.

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,13 @@ class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
   
-  validates_presence_of :body, :conversation_id, :user_id
+  validates_presence_of :body, :conversation_id, :user_id, length: { maximum: 1000 }
+
+  before_save :remove_whitespace
+
+  private
+
+  def remove_whitespace
+    self.body = body.strip
+  end
 end
