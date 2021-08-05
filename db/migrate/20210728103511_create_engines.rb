8c0e9f9a8818c77f4c0633bbdecb7a94993dd8e5
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-06 00:48:26 +1000

    Adds dismissable alerts and shared navbar and footer to application.html.erb

--- db/migrate/20210728103511_create_engines.rb
+++ db/migrate/20210728103511_create_engines.rb
@@ -2,7 +2,6 @@ class CreateEngines < ActiveRecord::Migration[6.1]
   def change
     create_table :engines do |t|
       t.string :name
-      t.decimal :displacement, precision: 10, scale: 1
 
       t.timestamps
     end
