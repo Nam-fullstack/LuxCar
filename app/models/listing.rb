6189c3c9adb44a7fec26edd748119766de415264
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-06 13:43:35 +1000

    Adds image upload configuration.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -17,13 +17,15 @@ class Listing < ApplicationRecord
   validates :mileage, numericality: { greater_than: 0 }
   validates :price, presence: true, numericality: { greater_than: 50000 }
   validates :state_id, presence: true
-  validates :title, length: { maximum: 50 }
+  validates :postcode, presence: true, length: { is: 4 }, numericality: { greater_than: 0200, less_than: 9999 }
+  validates :title, length: { maximum: 100 }
   # validates :variant_id, presence: true
   
   before_save :remove_whitespace
   before_validation :convert_price_to_cents, if: :price_changed?
+  before_save :create_variant
 
-  # @door_join = Door.left_joins :body_type
+  # @door_join = Door.left_joins(:body_type)
 
   private
 
@@ -34,5 +36,9 @@ class Listing < ApplicationRecord
 
   def convert_price_to_cents
     self.price = price * 100
+  end
+
+  def create_variant
+    @new_variant = { params[:listing][:year_id], params[:listing][:model_id], params[:listing][:door_id], params[:listing][:body_type_id], params[:listing][:speed_id], params[:listing][:transmission_id], params[:listing][:engine_id], params[:listing][:petrol_id] }
   end
 end
