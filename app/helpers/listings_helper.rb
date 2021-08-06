1a1638917460afa9cf30fbe181d8b7082393168f
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-07 03:35:00 +1000

    BUG.. trying to get Image upload to work. Invalid request parameters: expected Array (got String) for param image.

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,6 +1,6 @@
 module ListingsHelper
   def image_select(listing)
-    return listing.picture if listing.picture.attached?
+    return listing.pictures if listing.pictures.attached?
     return 'default-image.png' # use default image if no picture attached
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,21 @@ Rails.application.routes.draw do
   resources :variants
   resources :listings
   resources :events
-  # skip: :all - enables current_user helper methods 
   # devise_for :admins#, controllers: { sessions: 'admins/sessions' }
   devise_for :users#, controllers: { sessions: 'users/sessions' }
+  # skip: :all - enables current_user helper methods 
   # , path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }, skip: :all
 
   root to: 'pages#home'
   get 'pages/restricted'
-  # get '/success', to: "payments#success", as: "payment_success"
-  # post '/payments/webhook', to: "payments#webhook", as: "webhook"
-  # post '/payments', to: "payments#create_payment_intent", as: "create_payment_intent"
+  get 'messages/inbox'
+  get 'conversations/index'
+  post '/conversations', to: 'conversations#create', as: 'create_conversation'
+  get '/success', to: "payments#success", as: "payment_success"
+  post '/payments/webhook', to: "payments#webhook", as: "webhook"
+  post '/payments', to: "payments#create_payment_intent", as: "create_payment_intent"
+  post '/listings/(:id)', to: 'listings#update', as: 'update_listing'
+  delete '/listings/(:id)', to: 'listings#destroy', as: 'delete_listing'
+  post 'watches', to: 'watches#create', as: 'create_watch'
+  delete '/watches/(:id)', to: 'watches#destroy', as: 'delete_watch'
 end
