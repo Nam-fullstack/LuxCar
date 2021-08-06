5bad3f1a19cd739fc6c81a0c391a5638e48945ea
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-07 00:33:49 +1000

    Data Sanitization for Variant displacement, fuel consumption, power, safety rating, weight.

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -13,7 +13,20 @@ class Variant < ApplicationRecord
 
   # These should be optional, so might comment out validations here
   # and validate in form.
+  validates :displacement, numericality: { greater_than: 0, less_than: 20 }
   # validates :fuel_consumption, numericality: { greater_than: 0 }
   # validates :weight, numericality: { greater_than: 0 }
   # validates :safety_rating, numericality: { great_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+
+  before_save :remove_whitespace
+
+  private
+
+  def remove_whitespace
+    self.displacement = displacement.strip
+    self.fuel_consumption = fuel_consumption.strip
+    self.power = power.strip
+    self.safety_rating = safety_rating.strip
+    self.weight = weight.strip
+  end
 end
