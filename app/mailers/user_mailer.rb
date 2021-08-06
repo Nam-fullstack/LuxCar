fdd49db9c2756d847d99474031ce2bc6eafa49e3
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-06 22:26:53 +1000

    Sets up welcome emails when user signs up.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
-    # default from: ''
+    default from: 'notifications@luxurycar.herokuapp.com'
 
-    def welcome
+    def welcome_email
         @user = params[:user]
         @url = 'https://luxurycar.herokuapp.com/'
         mail(to: @user.email, subject: "Welcome to LuxuryCar! Find your dream now.")
