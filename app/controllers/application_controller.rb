98a02dc54951e11d35b15a575b67a84859e43afb
Nam-fullstack <namnguyen13_@hotmail.com>
2021-08-15 02:09:52 +1000

    Adds methods to application controller for non-logged in user to continue off where they were before they had to sign in/up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,44 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :authenticate_admin!
+  
+  # Stores the url before user had to sign in so they are redirected to where they
+  # wanted to go after they sign in/up.
+  before_action :store_user_location!, if: :storable_location? 
   before_action :authenticate_user!
   before_action :set_time_zone, if: :user_signed_in?
+  # before_action :authenticate_admin!
+
 
   # around_action :switch_time_zone, :if => :current_user
 
-  # def switch_time_zone(&block)
-  #   Time.use_zone(current_user.time_zone, &block)
-  # end
+  private
+
+  def switch_time_zone(&block)
+    # Time.use_zone(current_user.time_zone, &block)
+  end
 
   def set_time_zone
     # Time.zone = current_user.time_zone
+  end
+
+  private
+  # Its important that the location is NOT stored if:
+  # - The request method is not GET (non idempotent)
+  # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an 
+  #   infinite redirect loop.
+  # - The request is an Ajax request as this can lead to very unexpected behaviour.
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
   end
 
   protected
